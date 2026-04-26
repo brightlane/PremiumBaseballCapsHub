892b90204b6be810b03fbc7cfac17294d5d627ec
brightlane <benitopalmarini@aol.com>
2026-04-25 20:36:03 -0400

    Add trend generation function with predefined trends

---
+++ core/trends.py
@@ -0,0 +1,13 @@
+import random
+
+TREND_SEED = [
+    "baseball caps fashion 2026",
+    "army caps streetwear trend",
+    "snapback hats outfit ideas",
+    "camo caps military style",
+    "summer hats sun protection",
+    "trucker caps breathable fashion"
+]
+
+def get_trend():
+    return random.choice(TREND_SEED)
